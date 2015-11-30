8b72365d086b388fbe67cb4f571c57727266a9a7
Bennet Bartmann <bennet.bartmann@googlemail.com>
2015-12-01 00:01:41 +0100

    Update Title.py

--- Title.py
+++ Title.py
@@ -9,14 +9,14 @@ class Title(ModulePrototype):
         if url is not None:
             with open("links.txt", "ab") as links:
                 links.write(what + "\n")
+            if url == 'http://www.rehakids.de/phpBB2/ftopic112457.html':
+                defaultlib.defaultlib.send('PDF Adressen zur Autismusdiagnostik :: REHAkids Das Forum fuer besondere Kinder :: Das Forum fuer behinderte Kinder.')
+                return
             try:
                 content = urllib.urlopen(url.group("url")).read()
                 titleRE = re.compile("<title>(.+?)</title>")
                 title = titleRE.search(content).group(1)
                 defaultlib.defaultlib.send(title, Connection.channel)
             except:
-                if what == 'http://www.rehakids.de/phpBB2/ftopic112457.html':
-                    defaultlib.defaultlib.send('PDF Adressen zur Autismusdiagnostik :: REHAkids Das Forum fuer besondere Kinder :: Das Forum fuer behinderte Kinder.')
-                    return
                 if Connection.debug:
                     print("URL nicht parsebar: " + what)
